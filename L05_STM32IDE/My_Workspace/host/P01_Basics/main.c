bbf2c6dd4aca3bef4b7f876b2adb21a6af5efacd
Koushik467806 <koushikt85223@gmail.com>
2026-06-30 15:57:43 +0530

    Used debugging console and ITM to print using printf through port 0

---
+++ L05_STM32IDE/My_Workspace/host/P01_Basics/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: 26-Jun-2026
+ *      Author: def85
+ */
+
+
+#include <stdio.h>
+
+int main(void){
+	printf("Hello World!\n");
+
+	// Remove comment only when debugging so the console won't close immediately
+	// getchar();
+	return 0;
+}
